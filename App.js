7102f78a58f27e944cb6891f27385193df342c11
Suman Singh <sgh.suman@gmail.com>
2023-03-31 13:47:45 +0530

    updated

--- App.js
+++ App.js
@@ -1,19 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
-import { ScrollView, StyleSheet, Image, View, SafeAreaView } from 'react-native';
+import { ScrollView, StyleSheet, View, SafeAreaView } from 'react-native';
 import GraphWithLabel from './src/GraphWithLabels';
 
 export default function App() {
-  const logo = {
-    uri: 'https://reactnative.dev/img/tiny_logo.png',
-    width: 64,
-    height: 64,
-  };
-
   return (
     <SafeAreaView>
       <ScrollView style={{ height: "100%" }}>
         <View contentContainerStyle={styles.container}>
-          <StatusBar style="auto" />
           <GraphWithLabel />
         </View>
       </ScrollView>
